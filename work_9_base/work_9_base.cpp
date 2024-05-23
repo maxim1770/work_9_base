3031682eec4a5847f356c1dcb474fe9a45be9beb
maxim1770 <maxim1770@gmail.com>
2024-05-23 08:43:47 +0300

    Добавьте файлы проекта.

---
+++ work_9_base/work_9_base.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <array>
+
+const int ROWS = 3;
+const int COLS = 3;
+
+bool searchInMatrix(int matrix[ROWS][COLS], int target) {
+    int row = 0;
+    int col = COLS - 1;
+
+    while (row < ROWS && col >= 0) {
+        if (matrix[row][col] == target) {
+            return true;
+        }
+        else if (matrix[row][col] > target) {
+            col--;
+        }
+        else {
+            row++;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int matrix[ROWS][COLS] = { {1, 3, 5}, {7, 9, 11}, {13, 15, 17} };
+    int target = 9;
+
+    if (searchInMatrix(matrix, target)) {
+        std::cout << "Значение найдено в матрице." << std::endl;
+    }
+    else {
+        std::cout << "Значение не найдено в матрице." << std::endl;
+    }
+
+    return 0;
+}
+
+/*
++---------------------+
+| Начало программы    |
++---------------------+
+          |
+          V
++---------------------+
+| Инициализация       |
+| переменных          |
++---------------------+
+          |
+          V
++---------------------+
+| Установка начальных  |
+| значений row и col   |
++---------------------+
+          |
+          V
++---------------------+
+| Пока row < ROWS и    |
+| col >= 0             |
++---------------------+
+          |
+          V
++---------------------+
+| Если matrix[row][col]|
+| равно target,       |
+| вернуть true        |
++---------------------+
+          |
+          V
++---------------------+
+| Если matrix[row][col]|
+| больше target,      |
+| уменьшить col       |
++---------------------+
+          |
+          V
++---------------------+
+| Иначе                |
+| увеличить row        |
++---------------------+
+          |
+          V
++---------------------+
+| Вернуть false       |
++---------------------+
+          |
+          V
++---------------------+
+| Конец программы      |
++---------------------+
+*/
